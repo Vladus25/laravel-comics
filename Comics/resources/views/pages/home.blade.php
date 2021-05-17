19499566ca00e33d9d7a535545b2cf5e250d52c6
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-05-17 16:41:31 +0300

    Main del Comics done.

--- Comics/resources/views/pages/home.blade.php
+++ Comics/resources/views/pages/home.blade.php
@@ -8,6 +8,26 @@
     </section>
 
     {{-- Main del Comics --}}
+    <section class="main">
+      <div class="container">
+        <h1 class="logo-series">CURRENT SERIES</h1>
+
+        <div class="list-series">
+          <ul>
+            @foreach ($data as $d)
+              <li>
+                <img src="{{$d['thumb']}}" alt="logo">
+                <p>{{$d['series']}}</p>
+              </li>
+            @endforeach
+          </ul>
+        </div>
+
+        <div class="button-more">
+          <a href="#">LOAD MORE</a>
+        </div>
+      </div>
+    </section>
 
   </main>
 @endsection
